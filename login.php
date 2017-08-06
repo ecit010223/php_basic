b16d45fdd05b431fe36089403ffc875e48631f22
ecit010223 <ecit_eminem@163.com>
2017-08-06 21:33:24 +0800

    login格式化

--- login.php
+++ login.php
@@ -6,12 +6,25 @@
 	<body>
 	<form action="loginProcess.php" method="post">
 		<table align="center">		
-			<tr><td colspan="2" align="center"><h1>管理员登录</h1></td></tr>
-			<tr><td>帐&ensp;&ensp;号：</td><td><input type="text" name="user_id" value="<?php if(!empty($_COOKIE['user_id'])){echo $_COOKIE['user_id'];}?>"/></td></tr>
-			<tr><td>密&ensp;&ensp;码：</td><td><input type="password" name="passwd" value="<?php if(!empty($_COOKIE['passwd'])){echo $_COOKIE['passwd'];}?>"/></td></tr>
-			<tr><td colspan="2">是否要保存用户名和密码<input type="checkbox" name="cookie_chk"/></td></tr>
-			<tr align="center"><td colspan="2"><input type="submit" value="登陆"/></td></tr>
-			<tr align="center"><td colspan="2">
+			<tr>
+				<td colspan="2" align="center"><h1>管理员登录</h1></td>
+			</tr>
+			<tr>
+				<td>帐&ensp;&ensp;号：</td>
+				<td><input type="text" name="user_id" value="<?php if(!empty($_COOKIE['user_id'])){echo $_COOKIE['user_id'];}?>"/></td>
+			</tr>
+			<tr>
+				<td>密&ensp;&ensp;码：</td>
+				<td><input type="password" name="passwd" value="<?php if(!empty($_COOKIE['passwd'])){echo $_COOKIE['passwd'];}?>"/></td>
+			</tr>
+			<tr>
+				<td colspan="2">是否要保存用户名和密码<input type="checkbox" name="cookie_chk"/></td>
+			</tr>
+			<tr align="center">
+				<td colspan="2"><input type="submit" value="登陆"/></td>
+			</tr>
+			<tr align="center">
+				<td colspan="2">
 			<?php
 				if(!empty($_GET['error'])){
 					$error_no=$_GET['error'];
@@ -24,7 +37,8 @@
 					}
 				}
 			?>
-			</td></tr>
+				</td>
+			</tr>
 		</table>
 	</form>
 	</body>
